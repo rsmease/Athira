d7e3a7e12709125ceb35690eb191817fc2e15dd6
Ryan Mease <rsmease@gmail.com>
2018-03-22 12:48:01 -0700

    Update webpack options for v2 requirements, install webpack-cli

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,8 @@
 const path = require('path');
 const webpack = require("webpack");
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
+const plugins = []; // if using any plugins for both dev and production
 
-const prodPlugins = [
-    new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-        }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: true
-        }
-    })
-];
-
-plugins = plugins.concat(
-    process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
 
 module.exports = {
     context: __dirname,
@@ -31,9 +14,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '*']
     },
+    mode: 'development',
     plugins: plugins,
     module: {
-        loaders: [{
+        rules: [{
             test: /\.jsx?$/,
             exclude: /(node_modules|bower_components)/,
             loader: 'babel-loader',
