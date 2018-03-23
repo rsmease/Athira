a1c69e80f05e3532c5698abe66ad38e858b142f4
Ryan Mease <rsmease@gmail.com>
2018-03-23 11:12:42 -0700

    Update header to remove Link typo; add HashRouter and Provider to rootComponent

--- react/root.jsx
+++ react/root.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import createStore from './store/store';
+
+const rootComponent = ({ store }) => (
+    <Provider store={store}>
+        <HashRouter>
+            <h1>React is Still Runing</h1>
+        </HashRouter>
+    </Provider>
+)
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = createStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>React is Running</h1>, root);
+    ReactDOM.render(rootComponent(store), root);
 });
