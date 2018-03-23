69ca51795a15cc2d41ca73ad6bd9ecd606c8cff1
Ryan Mease <rsmease@gmail.com>
2018-03-23 08:07:26 -0700

    Update service, review partials to reflect updated index structure (jbuilders)

--- app/views/admin/reviews/_review.json.jbuilder
+++ app/views/admin/reviews/_review.json.jbuilder
@@ -1,1 +1,3 @@
-json.extract! review, :id, :title, :body, :reviewer_name, :reviewer_description, :reviewer_location, :other_image_url, :updated_at
+json.set! review.id do
+    json.extract! review, :id, :title, :body, :reviewer_name, :reviewer_description, :reviewer_location, :other_image_url, :updated_at
+end
--- react/root.jsx
+++ react/root.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as REVIEW from './ajax/reviews_ajax';
+import * as SERVICE from './ajax/services_ajax';
+
 document.addEventListener('DOMContentLoaded', () => {
+    window.getReview = REVIEW.getReview;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>React is Running</h1>, root);
 });
