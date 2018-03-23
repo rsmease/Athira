59266021a297bd0549ca164be354b8a7a6cfb32e
Ryan Mease <rsmease@gmail.com>
2018-03-23 08:44:58 -0700

    remove action creators from window; leave store

--- react/root.jsx
+++ react/root.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import * as REVIEW from './actions/review_actions';
-import * as SERVICE from './actions/service_actions';
+import createStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.getReview = REVIEW.getReview;
-    window.getAllReviews = REVIEW.getAllReviews;
-    window.getService = SERVICE.getService;
-    window.getAllServices = SERVICE.getAllServices;
+    const store = createStore();
+
 
     const root = document.getElementById('root');
     ReactDOM.render(<h1>React is Running</h1>, root);
