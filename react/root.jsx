9a629a513d7e5b378f47ceb255482982a75b0ab3
Ryan Mease <rsmease@gmail.com>
2018-03-23 08:48:21 -0700

    Update to root

--- react/root.jsx
+++ react/root.jsx
@@ -4,8 +4,6 @@ import createStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = createStore();
-
-
     const root = document.getElementById('root');
     ReactDOM.render(<h1>React is Running</h1>, root);
 });
