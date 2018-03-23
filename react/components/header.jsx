96a7ea61f16fc1d101c712455185a936f53afe6d
Ryan Mease <rsmease@gmail.com>
2018-03-23 11:48:23 -0700

    Add grid spec to navigation links

--- react/components/header.jsx
+++ react/components/header.jsx
@@ -13,9 +13,9 @@ class Header extends React.Component {
                     <Link to="/" className="header__navigation-link">Athira</Link>
                 </section>
                 <section className="header__navigation-link--container">
-                    <Link to="/about" className="header__navigation-link">About</Link>
-                    <Link to="/services" className="haeder__navigation-link">Services</Link>
-                    <Link to="/contact" className="haeder__navigation-link">Contact</Link>
+                    <Link to="/about" className="header__navigation-link--about">About</Link>
+                    <Link to="/services" className="haeder__navigation-link--services">Services</Link>
+                    <Link to="/contact" className="haeder__navigation-link--contact">Contact</Link>
                 </section>
             </header>
         );
