d464bd54510d26d018dc73dffcdf612a17ae751a
Ryan Mease <rsmease@gmail.com>
2018-03-27 12:44:33 -0700

    Adjust grid sizing of review index item; change webpack version

--- react/components/home/review_index_item.jsx
+++ react/components/home/review_index_item.jsx
@@ -5,9 +5,11 @@ class ReviewIndexItem extends React.Component {
     render() {
         return (
             <div className={`home__reviews--item-container--${this.props.alignment}`}>
+                <div></div>
                 <p className="home__reviews--body">{this.props.review.body}</p>
                 <h3 className="home__reviews--name">{this.props.review.reviewer_name}</h3>
                 <h3 className="home__reviews--description">{this.props.review.reviewer_description}</h3>
+                <div></div>
             </div>
         );
     }
