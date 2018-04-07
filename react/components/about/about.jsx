297b63ec26dbd5d926a2ff45f91090dfcfde6539
rsmease <rsmease@gmail.com>
2018-04-07 10:15:02 -0700

    Layout updates to summary section

--- react/components/about/about.jsx
+++ react/components/about/about.jsx
@@ -31,17 +31,15 @@ class About extends React.Component {
                 <section className="hero__container">
                 </section >
                 <section className="about__main--container">
-                    <section className="about__mixed--container">
-                        <div className="summary__container">
-                            <h2 className="summary__header-2">About Athira</h2>
-                            <h3 className="summary__header-3">{
-                                (this.props.company && this.props.company.long_description) ? this.props.company.long_description : ''
-                            }</h3>
-                        </div>
-                        <div className="home__leaders--items-container">
-                            {this.renderLeaders()}
-                        </div>
-                    </section>
+                    <div className="summary__container--with-title">
+                        <h2 className="summary__header-2">About Athira</h2>
+                        <h3 className="summary__header-3">{
+                            (this.props.company && this.props.company.long_description) ? this.props.company.long_description : ''
+                        }</h3>
+                    </div>
+                    <div className="home__leaders--items-container">
+                        {this.renderLeaders()}
+                    </div>
                     <Contact />
                 </section>
             </React.Fragment >
