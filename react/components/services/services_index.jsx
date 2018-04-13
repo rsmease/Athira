98a6017a0524cc3242e7948a267a8683ad88c925
rsmease <rsmease@gmail.com>
2018-04-13 06:08:02 -0700

    Add new stylesheet for services index; rename home style sheets with home modifier

--- react/components/services/services_index.jsx
+++ react/components/services/services_index.jsx
@@ -10,19 +10,7 @@ class ServicesIndex extends React.Component {
     }
 
     componentDidMount() {
-        this.props.getAllReviews();
         this.props.getAllServices();
-        this.props.getAllCompanies().then(console.log(this.props));
-    }
-
-    renderHeroBackground() {
-        if (this.props.company && this.props.company.splash_image_url) {
-            return {
-                'background-image': `url(${this.props.company.splash_image_url})`
-            };
-        } else {
-            return {};
-        }
     }
 
     renderServices() {
@@ -30,21 +18,8 @@ class ServicesIndex extends React.Component {
             return (
                 <React.Fragment>
                     {this.props.services.map(service => (
-                        <ServiceIndexItem key={service.id} service={service} />
+                        <ServicesIndexItem key={service.id} service={service} />
                     ))}
-                </React.Fragment>
-            )
-        }
-    }
-
-    renderReviews() {
-        if (this.props.reviews && this.props.reviews.length) {
-            return (
-                <React.Fragment>
-                    {this.props.reviews.map(function (review, i) {
-                        let alignment = (i % 2 === 0 ? 'left' : 'right');
-                        return <ReviewIndexItem key={review.id} review={review} alignment={alignment} />;
-                    })}
                 </React.Fragment>
             )
         }
@@ -53,25 +28,11 @@ class ServicesIndex extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <section className="hero__container"
-                    style={this.renderHeroBackground()}>
-                    <div className="hero__opacity-container">
-                        <h1 className="hero__header-1">{
-                            (this.props.company && this.props.company.headline) ? this.props.company.headline : ''
-                        }</h1>
-                    </div>
+                <section className="hero__container">
                 </section >
                 <section className="main__meta-container">
-                    <div className="summary__container">
-                        <h3 className="summary__header-3">{
-                            (this.props.company && this.props.company.short_description) ? this.props.company.short_description : ''
-                        }</h3>
-                    </div>
                     <div className="services-index__container--ServiceIndex">
                         {this.renderServices()}
-                    </div>
-                    <div className="reviews-index__container--ServiceIndex">
-                        {this.renderReviews()}
                     </div>
                 </section>
                 <Contact />
