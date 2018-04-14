fc2cc33b68dd392eb22d6784d99be46de342ba6a
rsmease <rsmease@gmail.com>
2018-04-14 08:25:45 -0700

    Add row separators for services in desktop view

--- react/components/services/services_index_item.jsx
+++ react/components/services/services_index_item.jsx
@@ -5,13 +5,16 @@ export class ServicesIndexItemLeft extends React.Component {
 
     render() {
         return (
-            <div className="services-index-item__container">
-                <div className="services-index-item__container--header">
-                    <Material.MdRemoveCircle className="services-index-item__icon" />
-                    <h3 className="services-index-item__header-3--name">{this.props.service.name}</h3>
+            <React.Fragment>
+                <div className="services-index-item__container">
+                    <div className="services-index-item__container--header">
+                        <Material.MdRemoveCircle className="services-index-item__icon" />
+                        <h3 className="services-index-item__header-3--name">{this.props.service.name}</h3>
+                    </div>
+                    <h3 className="services-index-item__header-3--description">{this.props.service.long_description}</h3>
                 </div>
-                <h3 className="services-index-item__header-3--description">{this.props.service.long_description}</h3>
-            </div>
+                <div className="border-maker"></div>
+            </React.Fragment>
         );
     }
 }
@@ -20,16 +23,19 @@ export class ServicesIndexItemRight extends React.Component {
 
     render() {
         return (
-            <div className="services-index-item__container">
-                <h3 className="services-index-item__header-3--description">{this.props.service.long_description}</h3>
-                <div className="services-index-item__container--header">
-                    <Material.MdRemoveCircle className="services-index-item__icon" />
-                    <h3 className="services-index-item__header-3--name">{this.props.service.name}</h3>
+            <React.Fragment>
+                <div className="services-index-item__container">
+                    <h3 className="services-index-item__header-3--description">{this.props.service.long_description}</h3>
+                    <div className="services-index-item__container--header">
+                        <Material.MdRemoveCircle className="services-index-item__icon" />
+                        <h3 className="services-index-item__header-3--name">{this.props.service.name}</h3>
+                    </div>
                 </div>
-            </div>
+                <div className="border-maker"></div>
+            </React.Fragment>
         );
     }
 }
 
-// export const ServicesIndexItemLeft;
+        // export const ServicesIndexItemLeft;
 // export const ServicesIndexItemRight;
