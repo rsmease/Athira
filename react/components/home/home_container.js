7ba5cce749d3daa1cc42c22c95a68e840d0f03cc
rsmease <rsmease@gmail.com>
2018-05-06 09:30:06 -0700

    Update home container

--- react/components/home/home_container.js
+++ react/components/home/home_container.js
@@ -8,17 +8,23 @@ import Home from './home';
 import * as ReviewActions from '../../actions/review_actions';
 import * as ServiceActions from '../../actions/service_actions';
 import * as CompanyActions from '../../actions/company_actions';
+import * as ImageCarouselActions from '../../ations/image_carousel_actions';
+import {
+    getAllImageCarousels
+} from '../../actions/image_carousel_actions';
 
 const mapStateToProps = (state, ownProps) => ({
     services: _.values(state.services),
     reviews: _.values(state.reviews),
-    company: _.values(state.companies)[0]
+    company: _.values(state.companies)[0],
+    imageCarousel: _.values(state.imageCarousels).filter(imageCarousel => imageCarousel.location === "home")
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     getAllServices: () => dispatch(ServiceActions.getAllServices()),
     getAllReviews: () => dispatch(ReviewActions.getAllReviews()),
-    getAllCompanies: () => dispatch(CompanyActions.getAllCompanies())
+    getAllCompanies: () => dispatch(CompanyActions.getAllCompanies()),
+    getAllImageCarousels: () => dispatch(ImageCarouselActions.getAllImageCarousels())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
