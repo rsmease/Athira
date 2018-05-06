17f57b1c0809ea925a4e6a27df99357495a09345
rsmease <rsmease@gmail.com>
2018-05-06 15:29:35 -0700

    Remove logger middleware

--- react/store/store.js
+++ react/store/store.js
@@ -11,5 +11,5 @@ import rootReducer from './root_reducer';
 export default (preloadedState = {}) => createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
 );
