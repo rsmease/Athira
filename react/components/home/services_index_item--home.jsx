f5602560ffa35b31237ab04e381fb445f4a0f75e
rsmease <rsmease@gmail.com>
2018-05-13 14:47:45 -0700

    Enlarge and recenter home service icons

--- react/components/home/services_index_item--home.jsx
+++ react/components/home/services_index_item--home.jsx
@@ -11,9 +11,9 @@ class ServicesIndexItem extends React.Component {
                 <div className="services-index-item__container--home">
                     <Link to={`services/#service-${this.props.service.id}`}>
                         <Material.MdRemoveCircle className="services-index-item__icon--home" />
-                        <h3 className="services-index-item__header-2--home">{this.props.service.name}</h3>
-                        <h3 className="services-index-item__header-3--home">{this.props.service.short_description}</h3>
                     </Link>
+                    <h3 className="services-index-item__header-2--home">{this.props.service.name}</h3>
+                    <h3 className="services-index-item__header-3--home">{this.props.service.short_description}</h3>
                 </div>
             </Fade>
         );
