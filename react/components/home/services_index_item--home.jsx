d2f86e872f7942e9e0b7d5f39c37c2fdc969f59d
Ryan <rmease@lumoslabs.com>
2018-06-02 13:33:29 -0700

    Add anchor route to services index item

--- react/components/home/services_index_item--home.jsx
+++ react/components/home/services_index_item--home.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import * as Material from 'react-icons/lib/md';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
 class ServicesIndexItem extends React.Component {
+
+    redirect(id) {
+        this.props.history.push(`/services#service-${id}`)
+    }
+
     render() {
+        console.log(this.props)
         return (
             <Fade bottom exit opposite>
-                <div className="services-index-item__container--home">
-                    <Material.MdRemoveCircle className="services-index-item__icon--home" />
-                    <h3 className="services-index-item__header-2--home">{this.props.service.name}</h3>
-                    <h3 className="services-index-item__header-3--home">{this.props.service.short_description}</h3>
-                </div>
+                <Link to={`/services#service-${this.props.service.id}`}>
+                    <div className="services-index-item__container--home">
+                        <Material.MdRemoveCircle className="services-index-item__icon--home" />
+                        <h3 className="services-index-item__header-2--home">{this.props.service.name}</h3>
+                        <h3 className="services-index-item__header-3--home">{this.props.service.short_description}</h3>
+                    </div>
+                </Link>
             </Fade>
         );
     }
 }
 
-export default ServicesIndexItem;
+export default withRouter(ServicesIndexItem);
