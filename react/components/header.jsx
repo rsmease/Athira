a916294e018bbfb9a10d4cc7da872d74c05ce20e
Ryan <rmease@lumoslabs.com>
2018-06-02 10:30:15 -0700

    Add header popup

--- react/components/header.jsx
+++ react/components/header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../app/assets/images/full-logo.png';
 import HamburgerMenu from 'react-hamburger-menu';
+import Popup from 'reactjs-popup';
 
 class Header extends React.Component {
     constructor() {
@@ -15,6 +16,12 @@ class Header extends React.Component {
         this.setState({
             open: !this.state.open
         });
+    }
+
+    closeHamburger() {
+        if (this.state.open) {
+            this.setState({ open: false })
+        }
     }
 
     toggleNavLinkView() {
@@ -37,17 +44,39 @@ class Header extends React.Component {
 
     renderHamburgerMenu() {
         return (
-            <HamburgerMenu
-                isOpen={this.state.open}
-                menuClicked={this.handleHamburgerClick.bind(this)}
-                width={36}
-                height={30}
-                strokeWidth={3}
-                rotate={0}
-                color='#FFDAA6'
-                borderRadius={0}
-                animationDuration={0.5}
-            />
+            <Popup
+                position="bottom right"
+                closeOnDocumentClick
+                trigger={
+                    <div>
+                        <HamburgerMenu
+                            isOpen={this.state.open}
+                            menuClicked={this.handleHamburgerClick.bind(this)}
+                            width={36}
+                            height={30}
+                            strokeWidth={3}
+                            rotate={0}
+                            color='#FFDAA6'
+                            borderRadius={0}
+                            animationDuration={0.5}
+                        />
+                    </div>
+                }
+                contentStyle={{
+                    border: '0',
+                    padding: '0',
+                }}
+                arrowStyle={{
+                    display: 'none'
+                }}
+                onClose={this.closeHamburger.bind(this)}
+            >
+                <div className="header__navigation-link--popup">
+                    <Link to="/about" className="header__navigation-link">About</Link>
+                    <Link to="/services" className="header__navigation-link">Services</Link>
+                    <Link to="/contact" className="header__navigation-link">Contact</Link>
+                </div>
+            </Popup>
         )
     }
 
