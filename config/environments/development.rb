14225099709b8b41b2fb24393707b8e0e17992b5
Ryan <rmease@lumoslabs.com>
2018-06-17 15:36:22 -0700

    Update mailer for active admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,11 +26,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.perform_caching = false
   config.action_mailer.default_options = { from: 'athira.mailer@gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
