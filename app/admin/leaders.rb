a087c66640d346f11c9c0d62bee3a31b3df62c0c
Ryan <rmease@lumoslabs.com>
2018-06-17 16:26:55 -0700

    Add leader management, update missing CSS

---
+++ app/admin/leaders.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Leader do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :title, :description, :headshot_url, :created_at, :updated_at
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
