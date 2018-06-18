1780fe165311f8ef9a560e94f33149c4f9991b6e
Ryan <rmease@lumoslabs.com>
2018-06-17 17:02:27 -0700

    Add AA panel for companies

---
+++ app/admin/companies.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Company do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :headline, :long_description, :short_description, :created_at, :updated_at, :splash_image_url
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
