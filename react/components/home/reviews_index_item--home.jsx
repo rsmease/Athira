46d75752cde14f4b9931d3424c66be07d23509e6
rsmease <rsmease@gmail.com>
2018-06-23 13:06:23 -0700

    Add css constraints to images

--- react/components/home/reviews_index_item--home.jsx
+++ react/components/home/reviews_index_item--home.jsx
@@ -13,7 +13,11 @@ class ReviewsIndexItem extends React.Component {
                     <h3 className="reviews-index-item__header-3--home">
                         -  {this.props.review.reviewer_name}
                     </h3>
-                    <img src={this.props.review.logo_url} alt={`${this.props.review.company} logo`} />
+                    <img
+                        className="reviews-index-item__image--home"
+                        src={this.props.review.logo_url}
+                        alt={`${this.props.review.company} logo`}
+                    />
                 </div>
             </Fade>
         );
