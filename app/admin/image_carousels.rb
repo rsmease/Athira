1034cd6fc1f6445e411fa978c0d070cd0932ebc2
rsmease <rsmease@gmail.com>
2018-07-14 15:50:38 -0700

    Allow for seralized input of image carousel URLs

--- app/admin/image_carousels.rb
+++ app/admin/image_carousels.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register ImageCarousel do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :location, :urls, :created_at, :updated_at, :headline
+    permit_params :location, :urls, :created_at, :updated_at, :headline
 #
 # or
 #
@@ -11,5 +11,20 @@ permit_params :location, :urls, :created_at, :updated_at, :headline
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+    form do |f|
+        f.inputs do
+            f.input :location
+            f.input :headline
+            f.inputs "Carousel URLs" do
+                f.input :serialized_urls, :as => :text, hint: "HINT: Save each URL on a separate line, separated by a line."
+            end
+        end
+        f.actions
+    end
 
+    before_save do |image_carousel|
+        unless params[:image_carousel].nil? || params[:image_carousel][:serialized_urls].nil?
+            image_carousel.urls = params[:image_carousel][:serialized_urls].split("\r\n\r\n")
+        end
+    end
 end
--- app/models/image_carousel.rb
+++ app/models/image_carousel.rb
@@ -1,3 +1,7 @@
 class ImageCarousel < ApplicationRecord
     validates :location, :urls, :headline, presence: true
+
+    def serialized_urls
+        self.urls.join("\n\n")
+    end
 end
