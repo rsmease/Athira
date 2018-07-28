332b40d24ce9eb01fd483b2298c62d0ec8c1cd2e
rsmease <rsmease@gmail.com>
2018-07-28 09:07:52 -0700

    update email target

--- app/mailers/visitor_mailer.rb
+++ app/mailers/visitor_mailer.rb
@@ -2,6 +2,6 @@ class VisitorMailer < ApplicationMailer
   def visitor
     @visitor = params[:visitor]
     email_title = 'Athira, you have a New Message from ' + @visitor.first_name + ' ' + @visitor.last_name + ' sent at ' + Time.now.to_s
-    mail(to: 'rsmease@gmail.com', subject: email_title)
+    mail(to: 'ria@athirasolutions.com', subject: email_title)
   end
 end
