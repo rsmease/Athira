262cad9f66eece8d877739255d1d0534fac67a00
rsmease <rsmease@gmail.com>
2018-07-28 10:32:34 -0700

    Style terms

--- react/components/terms/terms_index_item.jsx
+++ react/components/terms/terms_index_item.jsx
@@ -5,16 +5,17 @@ import Fade from 'react-reveal/Fade';
 class TermsIndexItem extends React.Component {
     render() {
         return (
-            <Fade bottom cascade>
-                <div className="terms-index-item__container">
+
+            <div className="terms-index-item__container">
+                <Fade bottom>
                     <h4 className="terms-index-item__header-4">
                         {this.props.title}
                     </h4>
                     <div className="terms-index-item__description" dangerouslySetInnerHTML={
                         { __html: this.props.text }
                     } />
-                </div>
-            </Fade>
+                </Fade>
+            </div>
         );
     }
 }
