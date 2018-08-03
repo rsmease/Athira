29859b2d3be4aaba0fb78778a3677bbae57ded6d
Ryan <rmease@lumoslabs.com>
2018-08-03 15:44:34 -0700

    Update certificate confirmaton string

--- app/controllers/encryptions_controller.rb
+++ app/controllers/encryptions_controller.rb
@@ -1,5 +1,5 @@
 class EncryptionsController < ApplicationController
   def certificate
-    render plain: '0I6ywkUSWiaXyLRPlLJ49Ph9ltPK72mN8h76vRkdF5k.DJ3-1k1tK8oN8KPWZe7foOZIaAuL4NB76VrhLZvjxPg'
+    render plain: '2OU3zHcGZMG0i1rHfxAB6Er_35BTpLeG3_tZpZC5MnQ.DJ3-1k1tK8oN8KPWZe7foOZIaAuL4NB76VrhLZvjxPg'
   end
 end
