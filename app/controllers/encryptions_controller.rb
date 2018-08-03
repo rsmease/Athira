9868fb96068da85d63a91a01ebd65bf6bb297201
Ryan <rmease@lumoslabs.com>
2018-08-03 15:34:39 -0700

    Change to plaintext render

--- app/controllers/encryptions_controller.rb
+++ app/controllers/encryptions_controller.rb
@@ -1,4 +1,5 @@
 class EncryptionsController < ApplicationController
   def certificate
+    render plain: '0I6ywkUSWiaXyLRPlLJ49Ph9ltPK72mN8h76vRkdF5k.DJ3-1k1tK8oN8KPWZe7foOZIaAuL4NB76VrhLZvjxPg'
   end
 end
