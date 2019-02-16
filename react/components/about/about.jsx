fd39a43713f79d65791ee28566b65f791924a675
rsmease <rsmease@gmail.com>
2019-02-15 17:37:35 -0800

    Remove animations, about page

--- react/components/about/about.jsx
+++ react/components/about/about.jsx
@@ -1,78 +1,83 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import * as Material from 'react-icons/lib/md';
+import React from "react";
+import { Link } from "react-router-dom";
+import * as Material from "react-icons/lib/md";
 
-import StaticHeroImage from '../hero/static_hero_image';
-import LeaderIndexItem from './leader_index_item';
-import Contact from '../contact/contact';
+import StaticHeroImage from "../hero/static_hero_image";
+import LeaderIndexItem from "./leader_index_item";
+import Contact from "../contact/contact";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 class About extends React.Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    componentWillMount() {
-        window.scroll(0, 0);
-    }
+  componentWillMount() {
+    window.scroll(0, 0);
+  }
 
-    componentDidMount() {
-        this.props.getAllLeaders();
-        this.props.getAllCompanies();
+  componentDidMount() {
+    this.props.getAllLeaders();
+    this.props.getAllCompanies();
+  }
 
+  setImageURL() {
+    if (this.props.company && this.props.company.about_image_url.length) {
+      return this.props.company.about_image_url;
+    } else {
+      return "";
     }
+  }
 
-    setImageURL() {
-        if (this.props.company && this.props.company.about_image_url.length) {
-            return this.props.company.about_image_url;
-        } else {
-            return "";
-        }
+  renderLeaders() {
+    if (this.props.leaders && this.props.leaders.length) {
+      return (
+        <React.Fragment>
+          {this.props.leaders.map(leader => (
+            <LeaderIndexItem
+              key={leader.id}
+              alignment={leader.id % 2 === 0 ? "left" : "right"}
+              leader={leader}
+            />
+          ))}
+        </React.Fragment>
+      );
     }
+  }
 
-    renderLeaders() {
-        if (this.props.leaders && this.props.leaders.length) {
-            return (
-                <React.Fragment>
-                    {this.props.leaders.map(leader => (
-                        <LeaderIndexItem
-                            key={leader.id}
-                            alignment={leader.id % 2 === 0 ? "left" : "right"}
-                            leader={leader}
-                        />
-                    ))}
-                </React.Fragment>
-            )
-        }
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <section className="hero__container vision-obstacles-execution-container">
-                    <StaticHeroImage imageURL={this.setImageURL()} />
-                </section>
-                <section className="main__meta-container">
-                    <Fade bottom cascade>
-                        <div className="summary__container--with-title">
-                            <h2 className="summary__header-2">About Athira</h2>
-                            <div className="summary__header-3" dangerouslySetInnerHTML={
-                                { __html: this.props.company && this.props.company.long_description ? this.props.company.long_description : '' }
-                            } />
-                        </div>
-                    </Fade>
-                    <div className="leaders-index__container">
-                        {this.renderLeaders()}
-                    </div>
-                    <Contact
-                        email_address={this.props.company ? this.props.company.email_address : ''}
-                        phone_number={this.props.company ? this.props.company.phone_number : ''}
-                    />
-                </section>
-            </React.Fragment >
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <section className="hero__container vision-obstacles-execution-container">
+          <StaticHeroImage imageURL={this.setImageURL()} />
+        </section>
+        <section className="main__meta-container">
+          <div className="summary__container--with-title">
+            <h2 className="summary__header-2">About Athira</h2>
+            <div
+              className="summary__header-3"
+              dangerouslySetInnerHTML={{
+                __html:
+                  this.props.company && this.props.company.long_description
+                    ? this.props.company.long_description
+                    : ""
+              }}
+            />
+          </div>
+          <div className="leaders-index__container">{this.renderLeaders()}</div>
+          <Contact
+            email_address={
+              this.props.company ? this.props.company.email_address : ""
+            }
+            phone_number={
+              this.props.company ? this.props.company.phone_number : ""
+            }
+          />
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default About;
--- react/components/about/leader_index_item.jsx
+++ react/components/about/leader_index_item.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import * as Typicon from 'react-icons/lib/ti';
+import React from "react";
+import * as Typicon from "react-icons/lib/ti";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 class LeaderIndexItem extends React.Component {
-    render() {
-        return (
-            <Fade
-                left={this.props.alignment === "left"}
-                right={this.props.alignment === "right"}
-                cascade
-            >
-                <div className="leader-index-item__container">
-                    <div className="leader-index-item__headshot" style={{ background: `url(${this.props.leader.headshot_url}) 50% 50% no-repeat`, backgroundSize: 'cover' }} alt={`${this.props.leader.name} headshot`} />
-                    <div className="leader-index-item__container--text">
-                        <h2 className="leader-index-item__header-2">Meet {this.props.leader.name}</h2>
-                        <div className="leader-index-item__header-3" dangerouslySetInnerHTML={
-                            { __html: this.props.leader.description }
-                        } />
-                    </div>
-                </div>
-            </Fade>
-        );
-    }
+  render() {
+    return (
+      <div className="leader-index-item__container">
+        <div
+          className="leader-index-item__headshot"
+          style={{
+            background: `url(${
+              this.props.leader.headshot_url
+            }) 50% 50% no-repeat`,
+            backgroundSize: "cover"
+          }}
+          alt={`${this.props.leader.name} headshot`}
+        />
+        <div className="leader-index-item__container--text">
+          <h2 className="leader-index-item__header-2">
+            Meet {this.props.leader.name}
+          </h2>
+          <div
+            className="leader-index-item__header-3"
+            dangerouslySetInnerHTML={{ __html: this.props.leader.description }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LeaderIndexItem;
